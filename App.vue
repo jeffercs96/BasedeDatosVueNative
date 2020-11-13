9e04facce05307682766ab044bd5d75119fbe545
Jefferson <jeffercs96@gmail.com>
2020-11-13 18:15:50 -0500

    App Base de datos VueNative

---
+++ App.vue
@@ -0,0 +1,53 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createMaterialTopTabNavigator,
+  createBottomTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import CameraScreen from "./screens/Camera.vue";
+import HomeScreen from "./screens/Home.vue";
+import WebServiceScreen from "./screens/WebService.vue";
+import DatabaseScreen from "./screens/Database.vue";
+//import ImageBrowserScreen from "./screens/ImageBrowser.vue";
+
+
+const Tabs = createMaterialTopTabNavigator({
+  Camera: HomeScreen,
+  API: WebServiceScreen,
+  DB: DatabaseScreen
+  // API2: ImageBrowserScreen,
+});
+
+const StackNavigator = createStackNavigator(
+  {
+    Tabs,
+    Camara: CameraScreen
+  },
+  {
+    defaultNavigationOptions: {
+      gestureEnabled: false,
+      title: "Fotos & Video",
+      headerTitleStyle: {
+        textAlign: "center",
+        alignSelf: "center",
+        color: "white"
+      },
+      headerStyle: {
+        backgroundColor: "black"
+      }
+    }
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+};
+</script>
